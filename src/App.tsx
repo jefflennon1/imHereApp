2b756aa1d71415889cb8feb172dc5869148173fd
jefflenno1 <jeffersonlennon1@outlook.com>
2023-06-19 14:41:18 -0300

    First commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,31 +1,35 @@
 import { ReactElement } from 'react'
-import { StyleSheet, Text } from 'react-native'
+import { StyleSheet, Text , Image} from 'react-native'
 import { SafeAreaProvider, SafeAreaView } from 'react-native-safe-area-context'
+
+
+
+export default function App(): ReactElement {
+  return (
+    <SafeAreaProvider>
+      <SafeAreaView style={styles.container}>
+        <Text style={styles.emoji}>🚀</Text>
+        <Text style={styles.text}>Os mags são um bando de esquerdo-machos</Text>
+        <Text>👌</Text>
+        </SafeAreaView>
+    </SafeAreaProvider>
+  )
+}
+
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#023020',
     alignItems: 'center',
     justifyContent: 'center'
   },
   text: {
-    marginBottom: 8
+    marginBottom: 8,
+    color: '#ffff'
   },
   emoji: {
     fontSize: 82,
     marginBottom: 24
   }
 })
-
-export default function App(): ReactElement {
-  return (
-    <SafeAreaProvider>
-      <SafeAreaView style={styles.container}>
-        <Text style={styles.emoji}>😻</Text>
-        <Text style={styles.text}>Open src/App.tsx to start working on your app!</Text>
-        <Text>Happy hacking! 🙌🏻</Text>
-      </SafeAreaView>
-    </SafeAreaProvider>
-  )
-}
