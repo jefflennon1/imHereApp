191f4dbc3b1cae46c1643a3cdca926881526259d
jefflenno1 <jeffersonlennon1@outlook.com>
2023-07-26 16:18:55 -0300

    styles of statusbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { ReactElement } from 'react' 
-import Home from './screens/Home'
+import { ReactElement } from 'react';
+import { StatusBar } from 'react-native';
+import Home from './screens/Home';
 
 
 export default function App(): ReactElement {
   return (
-    <Home />
+    <>
+      <StatusBar
+        barStyle={'light-content'}
+        translucent
+      />
+      <Home />
+    </>
   )
 }
- 
